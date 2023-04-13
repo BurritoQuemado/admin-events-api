abb1e0e6a9c41b63e9f4766d2e8b772540311806
Diego Flores <dfmonroy07@gmai.com>
2023-04-13 09:19:55 -0600

    ready for prod

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL || 'postgresql://@localhost:5432/events',
-        ssl: process.env.DATABASE_URL ? true : false
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
